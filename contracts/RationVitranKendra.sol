2e6dcf1c73859cff0838b4fc9d95ee67b50fbb7b
nlgupta-systango <nlgupta@isystango.com>
2022-04-26 19:34:51 +0530

    updated

--- contracts/RationVitranKendra.sol
+++ contracts/RationVitranKendra.sol
@@ -10,10 +10,18 @@ contract RationVitranKendra {
     uint8 public riceDistribute=5;
     uint8 public wheatDistribute=5;
     uint public nextDueTimePeriod=2592000;// 30 days =2592000 sec.
+
+    struct itemsPrice{
+        uint pulsesPrice;
+        uint ricePrice;
+        uint wheatPrice;
+    }
+
     struct PulsesStr {
         uint256 time;
         uint256 nextDue;
         uint userPulses;
+        // uint price;
     }
 
     struct RiceStr {
@@ -41,19 +49,25 @@ contract RationVitranKendra {
         address charityAddress;
         uint8 charityShare;
         bool isActive;
+        uint time;
+        uint nextDueTime;
     }
     
     mapping(address => User) public userMap;
     mapping(address => Charity) public charityMap;
     enum rawItems{pulses,rice,wheat}
+    itemsPrice public ip;
     constructor() {
         owner = msg.sender;
+        updateItemsPrice(20000000000000,15000000000000,10000000000000);
     }
     event registerUserEvent(address adsOfUser,bool isActive);
     event withdrawEvent(address adsOfUser,bool hasTaken,string item);
     event userRemovingEvent(address adsOfUser,bool isActive);
+    event updateItemsPriceEvent(uint pulsesPrice,uint ricePrice,uint wheatPrice,string msg);
    
     function registerUser(address adsOfUser) public onlyOwner {
+        require(userMap[adsOfUser].isActive==false,"RationVitranKendra: user is already registered");
         PulsesStr memory tempPulses = PulsesStr(0, 0,0);
         RiceStr memory tempRice = RiceStr(0, 0,0);
         WheatStr memory tempWheat = WheatStr(0, 0,0);
@@ -64,30 +78,29 @@ contract RationVitranKendra {
 
     function withDrawRation(rawItems item) public payable {
         require(userMap[msg.sender].isActive, "RationVitranKendra: not registered");
-        require(msg.value>=10000,"RationVitranKendra: provide Required amount");
         if(item==rawItems.pulses){
+            require(msg.value>ip.pulsesPrice,"RationVitranKendra: provide required amount for pulses");
             _withDrawPulses();
             emit withdrawEvent(msg.sender,true,"pulses");
 
         }else if(item==rawItems.rice){
+            require(msg.value>ip.ricePrice,"RationVitranKendra: provide required amount for rice");
             _withDrawRice();
             emit withdrawEvent(msg.sender,true,"rice");
 
-
         }else if(item==rawItems.wheat){
+            require(msg.value>ip.pulsesPrice,"RationVitranKendra: provide required amount for wheat");
             _withDrawWheat();
             emit withdrawEvent(msg.sender,true,"wheat");
-
         }
     }
         
-
     function _withDrawPulses() internal {
          require(
             block.timestamp > userMap[msg.sender].pulsesInfo.nextDue,
             "RationVitranKendra: early pulses withdraw request issue"
         );
-        require(totalPulses>=5,"RationVitranKendra: insufficient pulses ");
+        require(totalPulses>=pulsesDistribute,"RationVitranKendra: insufficient pulses ");
         userMap[msg.sender].pulsesInfo.userPulses+=pulsesDistribute;
         totalPulses-=pulsesDistribute;
         // 30 days in second=nextDueTimePeriod
@@ -100,7 +113,7 @@ contract RationVitranKendra {
             block.timestamp > userMap[msg.sender].riceInfo.nextDue,
             "RationVitranKendra: early rice withdraw request issue"
         );
-        require(totalRice>=5,"RationVitranKendra: insufficient rice ");
+        require(totalRice>=riceDistribute,"RationVitranKendra: insufficient rice ");
         userMap[msg.sender].riceInfo.userRice+=riceDistribute;
         totalRice-=riceDistribute;
         // 30 days in second=nextDueTimePeriod
@@ -113,7 +126,7 @@ contract RationVitranKendra {
             block.timestamp > userMap[msg.sender].wheatInfo.nextDue,
             "RationVitranKendra: early wheat withdraw request issue"
         );
-        require(totalWheat>=5,"RationVitranKendra: insufficient wheat ");
+        require(totalWheat>=wheatDistribute,"RationVitranKendra: insufficient wheat ");
         userMap[msg.sender].wheatInfo.userWheat+=wheatDistribute;
         totalWheat-=wheatDistribute;
         // 30 days in second=nextDueTimePeriod
@@ -122,16 +135,20 @@ contract RationVitranKendra {
     }
 
     function removeUser(address toRemoveUser) public onlyOwner {
+        require(userMap[toRemoveUser].isActive==true,"RationVitranKendra: user is not in registered list");
         userMap[toRemoveUser].isActive=false;
         emit userRemovingEvent(toRemoveUser,false);
        
     }
 
     function charityRegistration(string memory _charityName,address _charityAddress,uint8 _charityShare) public onlyOwner{
+        require(charityMap[_charityAddress].isActive==false,"RationVitranKendra: charity already registered");
         charityMap[_charityAddress].charityName=_charityName;
         charityMap[_charityAddress].charityAddress=_charityAddress;
         charityMap[_charityAddress].charityShare=_charityShare;
         charityMap[_charityAddress].isActive=true;
+        charityMap[_charityAddress].time=block.timestamp;
+        charityMap[_charityAddress].nextDueTime=block.timestamp;
     }
 
     function removeCharity(address toRemoveCharity) public onlyOwner{
@@ -142,9 +159,12 @@ contract RationVitranKendra {
 
     function withDrawFund() public {
         require(charityMap[msg.sender].isActive==true,"RationVitranKendra: not Registered charity");
-        require(address(this).balance>0);
+        require(block.timestamp>charityMap[msg.sender].nextDueTime,"RationVitranKendra: early withdraw request issue");
+        require(address(this).balance>0,"RationVitranKendra: does not have fund");
         address payable charityAddress=payable(msg.sender);
         charityAddress.transfer((address(this).balance*charityMap[msg.sender].charityShare)/100);
+        charityMap[msg.sender].time=block.timestamp;
+        charityMap[msg.sender].nextDueTime=block.timestamp+nextDueTimePeriod;
     }
 
     function refilling(uint _pulses,uint _rice,uint _wheat) public onlyOwner{
@@ -165,6 +185,14 @@ contract RationVitranKendra {
 
     function changeDistributionTime(uint newtime)public onlyOwner{
         nextDueTimePeriod=newtime;
+    }
+
+    function updateItemsPrice(uint _pulsesPrice,uint _ricePrice,uint _wheatPrice) public onlyOwner{
+        ip.pulsesPrice=_pulsesPrice;
+        ip.ricePrice=_ricePrice;
+        ip.wheatPrice=_wheatPrice;
+        emit updateItemsPriceEvent(_pulsesPrice,_ricePrice,_wheatPrice,"items price is updated");
+
     }
     
     modifier onlyOwner() {
---
+++ contracts/erc20.sol
@@ -0,0 +1,83 @@
+pragma solidity ^0.8.6;
+abstract contract IRVK{
+    
+    function name() public virtual view returns (string memory);
+    function symbol() public virtual view returns (string memory);
+    function decimals() public virtual view returns (uint8);
+    function totalSupply() public virtual view returns (uint256);
+    function balanceOf(address _owner) public virtual view returns (uint256 balance);
+    function transfer(address _to, uint256 _value) public virtual returns (bool success);
+    function transferFrom(address _from, address _to, uint256 _value) public virtual returns (bool success);
+    function approve(address _spender, uint256 _value) public virtual returns (bool success);
+    function allowance(address _owner, address _spender) public virtual view returns (uint256 remaining);
+    function x()public{
+
+    }
+
+}
+contract RVK is IRVK{
+    string internal _name;
+    string internal _symbol;
+    uint8 internal _decimals;
+    uint public _totalSupply;
+    address public _minter;
+    mapping(address=>uint) balances;
+    constructor(){
+        _name="RationVitranKendra";
+        _symbol="RVK";
+        _decimals=1;
+        _totalSupply=200;
+        _minter=0x7Fd333E334D0459d742ed31D82CC6b8f7353763D;
+        balances[_minter]=_totalSupply;
+    }
+
+    function name() public override view returns (string memory){
+         return _name;
+    }
+
+    function symbol() public override view returns (string memory){
+        return _symbol;
+
+    }
+    
+    function decimals() public override view returns (uint8){
+        return _decimals;
+    }
+
+    function totalSupply() public override view returns (uint256){
+        return _totalSupply;
+
+    }
+
+    function balanceOf(address _owner) public override view returns (uint256 balance){
+        return balances[_owner];
+    }
+
+     function transferFrom(address _from, address _to, uint256 _value) public override returns (bool success) {
+        require(balances[_from] >= _value);
+        balances[_from] -= _value; 
+        balances[_to] += _value;
+        return true;
+    }
+    
+
+    function transfer(address _to, uint256 _value) public override returns (bool success) {
+        return transferFrom(msg.sender, _to, _value);
+    }
+
+    function approve(address _spender, uint256 _value) public override returns (bool success) {
+        return true;
+    }
+
+    function allowance(address _owner, address _spender) public override view returns (uint256 remaining) {
+        return 0;
+    }
+
+    function mint(uint amount) public returns (bool) {
+        require(msg.sender == _minter);
+        balances[_minter] += amount;
+        _totalSupply += amount;
+        return true;
+    }
+
+}
